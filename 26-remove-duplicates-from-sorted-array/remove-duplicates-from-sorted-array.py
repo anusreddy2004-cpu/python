4785f7192052c7a07bc5dfb039be5ad5b3bb427c
Anu S <anusreddy2004@gmail.com>
2026-03-16 12:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (35.63%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,7 +4,6 @@ class Solution:
        n=len(nums)
        for k in range(1,n):
          if nums[k]!=nums[i]:
-            nums[i+1]=nums[k]
-            i+=1
+          nums[i+1]=nums[k]
+          i+=1
        return i+1
-
